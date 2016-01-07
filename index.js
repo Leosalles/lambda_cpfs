7f6d5f3f41479aaa07c83a14cc08e8204b3f96a9
Michael Belfer Bernstein <michael.bernstein@clicksign.com>
2016-01-07 04:27:18 -0200

    wip

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var config = require('./config.json')
 var AWS = require('aws-sdk');
 var uuid = require('node-uuid');
 var request = require('request');
+var https = require('https');
 var cheerio = require('cheerio');
 var fs = require('fs');
-
+  var async = require('async');
 
 AWS.config.region = 'us-east-1';
-var lambda = new AWS.Lambda({ apiVersion: '2015-03-31' });
 var dynamo = new AWS.DynamoDB.DocumentClient()
 var ses = new AWS.SES({apiVersion: '2010-12-01'});
 
@@ -72,83 +72,125 @@ var mandrill = function(event, context) {
   });
 };
 
-var clicksign_search = function(event, context){
+var process_stream = function(event, context){
   event.Records.forEach(function(record) {
     console.log(record.eventID);
     console.log(record.eventName);
     console.log('DynamoDB Record: %j', record.dynamodb);
 
-    bounced_email = record.dynamodb.Keys.email;
+    async.waterfall([
+      function getBouncedEmail(next){
+        bounced_email = record.dynamodb.NewImage.email.S;
+        if (bounced_email){
+          console.log('bounced_email: ', bounced_email);
+          next(null, bounced_email);
+        } else {
+          next(new Error('No bounced_mail found.'));
+        }
+      },
 
-    if (bounced_email){
-      console.log('bounced_mail: ', bounced_email);
-      sender = searchClicksign(bounced_email);
+      function getClicksignInvite(bounced_email, next){
+        var username = config['clicksign_auth_user'],
+            password = config['clicksign_auth_pass'],
+            url = 'https://' + username + ':' + password + '@desk.clicksign.com/admin/invites?email=' + bounced_email.replace('@', '%40');
+        console.log('url: ', url);
+        request({url: url}, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log('body: ', body);
+            next(null, bounced_email, body);
+          } else {
+            next(error);
+          }
+        });
+      },
 
-      if (sender){
-        sendEmail(sender, bounced_email);
+      function getArchiveFromInvite(bounced_email, invitesBody, next){
+        var body = cheerio.load(invitesBody);
+        var archive = body('a[href*="/admin/archives"]').first().text();
+        console.log('archive: ', archive);
+
+        if (archive){
+          next(null, bounced_email, archive)
+        } else {
+          next(new Error('Error in getArchiveFromInvite.'));
+        }
+      },
+
+      function getClicksignArchive(bounced_email, archive, next){
+        //REVISAR A PARTIR DAQUI. ACIMA ESTÁ FUNCIONANDO.
+        var username = config['clicksign_auth_user'],
+            password = config['clicksign_auth_pass'],
+            url = 'https://' + username + ':' + password + '@desk.clicksign.com/admin/archives/' + archive;
+        console.log('url: ', url);
+        request({url: url}, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log('body: ', body);
+            next(null, bounced_email, body);
+          } else {
+            next(error);
+          }
+        });
+
+        //$ = cheerio.load(url_body);
+        //sender = {
+        //  "name": $('caption').text().split('por ')[1].trim().slice(0,-1).split(' (')[0],
+        //  "mail": $('caption').text().split('por ')[1].trim().slice(0,-1).split(' (')[1]
+        //};
+        //console.log('sender: ', sender);
+        //return sender
+      //},
+
+      //function checkIfWasSent(sender, next){
+
+      //},
+
+      //function sendEmail(sender, bounced_email){
+      //  if (sender){
+      //    // dev mode
+      //    sender['email'] = 'mb@clicksign.com'
+
+      //    // template
+      //    var template = '';
+      //    fs.readFile("mail_template.html", function (err, data) {
+      //      if (err) throw err;
+      //      template = data.toString();
+      //    });
+      //    template = template.replace('{sender_name}', sender['name']);
+      //    template = template.replace('{sender_email}', sender['email']);
+      //    template = template.replace('{bounced_email}', bounced_email);
+
+      //    var params = {
+      //       Source: config['ses_from'],
+      //       Destination: { ToAddresses: sender_email },
+      //       Message: {
+      //         Subject: { Data: 'E-mail inválido em seu documento' },
+      //         Body: { Html: { Data: template } }
+      //       }
+      //     };
+
+      //     ses.sendEmail(params, function(err, data){
+      //       if(err) throw err
+      //       console.log('Email sent:');
+      //       console.log(data);
+      //     });
+      //   } else {
+      //     console.log('Bounce with no sender: ', bounced_email);
+      //   }
+       }
+    ], function (err, result) {
+      if (err) {
+        console.error('Failed to publish notifications: ', err);
       } else {
-        console.log('Bounce with no sender: ', bounced_email);
+        console.log('Successfully published all notifications.');
+        //context.succeed("function clicksign_search succeed!");
+        //context.done();
       }
-    } else {
-      console.log('No bounced_mail found.');
-    }
+    });
   });
 
-  context.succeed("function clicksign_search succeed!");
-  context.done();
 };
 
-function searchClicksign(bounced_email){
-  //fatch invite
-  url = "https://desk.clicksign.com/admin/invites?email=" + bounced_email.replace('@', '%40');
-  body = request.get(url).auth(config['clicksign_auth_user'], config['clicksign_auth_pass']);
-  $ = cheerio.load(body);
-  var archive = $('a[href*="/admin/archives"]', body)[0].text;
-  console.log('archive: ', archive);
 
-  if (archive != ''){
-    //fatch archive
-    url = "https://desk.clicksign.com/admin/archives/" + archive;
-    body = request.get(url).auth(config['clicksign_auth_user'], config['clicksign_auth_pass']);
-    $ = cheerio.load(body);
-    sender = {
-      "name": $('caption').text().split('por ')[1].trim().slice(0,-1).split(' (')[0],
-      "mail": $('caption').text().split('por ')[1].trim().slice(0,-1).split(' (')[1]
-    };
-    console.log('sender: ', sender);
-    return sender
-  }
-}
-
-function sendEmail(sender, bounced_email){
-  // dev mode
-  sender['email'] = 'mb@clicksign.com'
-
-  // template
-  var template = '';
-  fs.readFile("mail_template.html", function (err, data) {
-    if (err) throw err;
-    template = data.toString();
-  });
- template = template.replace('{sender_name}', sender['name']);
- template = template.replace('{sender_email}', sender['email']);
- template = template.replace('{bounced_email}', bounced_email);
-
- var params = {
-    Source: config['ses_from'],
-    Destination: { ToAddresses: sender_email },
-    Message: {
-      Subject: { Data: 'E-mail inválido em seu documento' },
-      Body: { Html: { Data: template } }
-    }
-  };
-
-  ses.sendEmail(params, function(err, data){
-    if(err) throw err
-    console.log('Email sent:');
-    console.log(data);
-  });
-}
 
 exports.handler = function(event, context) {
   console.log('Received event:', JSON.stringify(event, null, 2));
@@ -156,7 +198,7 @@ exports.handler = function(event, context) {
   var operation = '';
 
   if (event.Records){
-    operation = 'clicksign_search';
+    operation = 'process_stream';
   } else if (event.operation){
     operation = event.operation;
     delete event.operation;
@@ -171,8 +213,8 @@ exports.handler = function(event, context) {
     case 'mandrill':
       mandrill(event, context);
       break;
-    case 'clicksign_search':
-      clicksign_search(event, context);
+    case 'process_stream':
+      process_stream(event, context);
       break;
     default:
       context.fail(new Error('Unrecognized operation "' + operation + '"'));
